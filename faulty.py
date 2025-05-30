2f292f1f8e6321dbdd79b7a4da022914aacc3611
Chen Jiajun Philip <chenjiajunphilip@gmail.com>
2025-05-30 16:29:35 +0800

    Create faulty.py
    faulty, do not use

---
+++ faulty.py
@@ -0,0 +1,67 @@
+import re
+
+text_to_search = '''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+\d
+Ha HaHa
+
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+
+coreyms.com
+
+321-555-4321
+123.555.1234
+123*555*1234
+800-555-1234
+900-555-1234
+
+Mr. Schafer
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+'''
+
+sentence = 'Start a sentence and then bring it to an end'
+user_input1 = input("which is your source text? text_to_search or sentence? ")
+user_input2 = input("what do you want to search for? ")
+#x = re.compile(r'coreyms\.com')
+
+def to_raw(string):
+    return fr"{string}"
+
+
+x = re.compile(to_raw(user_input2))
+#y=x.finditer(text_to_search)
+y=x.finditer(user_input1)
+for i in y:
+    print(i)
+    k = 24
+    #print(str(i)[24:k])
+    num1 = ''
+    num2 = ''
+    s1 = str(i)[k:]
+    for j in s1:
+        #print(j)
+        if j.isnumeric():
+            num1 += j
+            #print(num1)
+            k+=1
+        else:
+            break
+    num1 = int(num1)
+    #m = k+3
+    s2 = str(i)[k+2:]
+    for l in s2:
+        if l.isnumeric():
+            num2 += l
+        else:
+            break
+    num2 = int(num2)
+    print(num1)
+    print(num2)
+    #print(text_to_search[num1:num2])
+    print(user_input1[num1:num2])
